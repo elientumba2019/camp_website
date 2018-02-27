0856c14270e5292c93f25e408812e5e21be75328
Elie Ntumba <elientumba@live.fr>
2018-02-27 11:09:07 +0000

    Campground route added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 
+//setting the view engine
+app.set("view engine" , "ejs");
+
 
 //landing page route
 app.get('/' , function(req , res) {
-    res.send("TODO : implement landing page");
+    res.render("landing");
 });
 
 
